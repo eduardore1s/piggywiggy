b1095802e755f9710b6f0631bbcfc6b35a411750
Eduardo Reis <edsilveira.reis@gmail.com>
2023-10-25 21:48:33 -0300

    first commit

---
+++ src/main/java/com/salomeandreis/piggywiggy/PiggyWiggyApplication.java
@@ -0,0 +1,13 @@
+package com.salomeandreis.piggywiggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyWiggyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggyWiggyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salomeandreis/piggywiggy/PiggyWiggyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salomeandreis.piggywiggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggyWiggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
